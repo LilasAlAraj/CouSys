bab5c0222848557b8e4dc4344c195b7e863ce310
LilasAlAraj <lilas.alaraj.9@gmail.com>
2021-08-09 19:02:38 +0300

    first with leen

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::view('loginpage',view('login'));
+Route::post('islogin','Admincontrol@adminloged');
