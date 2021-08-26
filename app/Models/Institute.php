2597d776db2e7ec30e2e101a60ee7a5be339aff2
LilasAlAraj <lilas.alaraj.9@gmail.com>
2021-08-26 23:18:50 +0300

    2L

---
+++ app/Models/Institute.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Institute extends Model
+{
+
+    use HasFactory;
+    protected $guarded = ['id'];
+    protected $table = 'institute';
+
+ //   protected $casts=['isAccepted'=>'boolean'];
+
+}
